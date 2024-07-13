bd9377ed1559bf7b5c8b5722cf847f23f01819ac
Simone <skixmix@gmail.com>
2024-07-13 16:16:23 +0200

    style: changed stylings and added resume info

--- website/assets/main.js
+++ website/assets/main.js
@@ -1,25 +1,31 @@
 async function walkTree(root, visit) {
-  await visit(root);
-  for (const node of root.childNodes) {
-    await walkTree(node, visit);
-  }
+	await visit(root);
+	for (const node of root.childNodes) {
+		await walkTree(node, visit);
+	}
 }
 
-addEventListener("DOMContentLoaded", function () {
-  const globalContainer = document.querySelector("#global-container");
+addEventListener('DOMContentLoaded', function () {
+	const globalContainer = document.querySelector('#global-container');
 
-  walkTree(globalContainer, function (el) {
-    const style = el.style;
-    if (!!style) {
-      style.visibility = "hidden";
-    }
-  });
+	walkTree(globalContainer, function (el) {
+		const style = el.style;
+		if (!!style) {
+			style.visibility = 'hidden';
+		}
+	});
 
-  walkTree(globalContainer, function (el) {
-    const style = el.style;
-    if (!!style) {
-      style.visibility = "visible";
-    }
-    return new Promise((resolve) => setTimeout(resolve, 1));
-  });
+	walkTree(globalContainer, function (el) {
+		const style = el.style;
+		if (!!style) {
+			style.visibility = 'visible';
+		}
+		return new Promise((resolve) => setTimeout(resolve, 1));
+	});
+
+	console.log('%cHello There!', 'color: #7DCFFF; font-size: 20px; font-weight: bold;');
+	console.log(
+		"%cI'm glad you're here! If you like my website, you can find the source here: https://github.com/skixmix/personal-website \n\nHave a nice day ;)",
+		'font-size: 14px',
+	);
 });
--- website/content/portfolio.md
+++ website/content/portfolio.md
@@ -3,9 +3,4 @@ layout: portfolio
 title: Portfolio
 ---
 
-This is my blog about my personal journey in learning programming & web
-development. It is an outlet for me to process and communicate what I’m
-learning, and also an opportunity to interact with other coders.
-
-I am excited about clean code, open source software, and learning/teaching. I
-love refactoring code, run applications in Docker, and streamline my vim config.
+Still working on it :)
